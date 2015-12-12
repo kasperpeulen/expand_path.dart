36401d59f366a5e657abd84012621c292757e124
Kasper Peulen <kasperpeulen@gmail.com>
2015-12-12 01:30:15 +0100

    docs: add docs for expandPath function

--- lib/expand_path.dart
+++ lib/expand_path.dart
@@ -1,5 +1,11 @@
 import 'package:homedir/homedir.dart';
 
+/// Expand a `~` character in a [path] to the home directory.
+///
+///     expandPath('~') == '/Users/kasper'
+///
+///     expandPath('~/file.txt') == '/Users/kasper/file.txt'
+///
 String expandPath(String path) {
   if (!path.startsWith('~')) {
     throw new ArgumentError('path can only be expanded if it starts with ~');
